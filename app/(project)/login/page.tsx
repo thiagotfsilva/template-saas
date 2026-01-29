3d21e3f3ccc5314b7bdfc4570d357c48b6548bc8
thiagotfsilva~ <thiagophsilva@hotmail.com>
2026-01-28 23:13:25 -0300

    ajuste login

--- app/(project)/login/page.tsx
+++ app/(project)/login/page.tsx
@@ -1,6 +1,15 @@
 import { handleAuth } from "@/app/actions/handleAuth";
+import { auth } from "@/app/lib/auth";
+import { redirect } from "next/navigation";
 
-const Login = () => {
+const Login = async () => {
+
+  const session = await auth();
+
+  if (session) {
+    redirect('/');
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-10">Login</h1>
